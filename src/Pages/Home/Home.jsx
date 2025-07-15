71d01d7c0c564509bb5d19112318f15908177f25
yemisrach-19 <yemi.t.maki@gmail.com>
2025-07-14 21:25:12 -0800

    Row collection

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Banner from "../../Components/Banner/Banner";
+import RowList from "../../Components/All rows/RowList/RowList";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Banner />
-
+      <RowList />
       <Footer />
     </div>
   );
